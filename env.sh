d4856fc7134e9daf2da4d641a8ac23bb86351958
Nate Diven <natediven1@gmail.com>
2025-07-07 20:00:43 -0600

    update env.sh

--- env.sh
+++ env.sh
@@ -1,13 +1,6 @@
-export EDITOR='nvim'
-export GIT_EDITOR='nvim'
+export EDITOR='hx'
+export GIT_EDITOR='hx'
 
-alias of='FILE=$(fdfind . --exclude node_modules | fzf) && [ -n "$FILE" ] && nvim "$FILE"'
+alias of='FILE=$(fd . --exclude node_modules | fzf) && [ -n "$FILE" ] && hx "$FILE"'
 alias ns='nix-shell'
-
-# setup brew
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"
 
