a7dae625ba42fe66df1bf33b6dc8f29856cf6194
Nate Diven <natediven1@gmail.com>
2025-08-04 13:33:53 -0600

    add alias for deleting node_modules folders

--- env.sh
+++ env.sh
@@ -7,6 +7,8 @@ alias tn='tmux new-session -s'
 alias tl='tmux list-sessions'
 alias ta='tmux attach-session'
 alias lg='lazygit'
+# recursively deletes all folders named "node_modules" in the current directory
+alias rmnm='find . -type d -name node_modules -prune -exec rm -rf {} +'
 
 # dotnet
 export DOTNET_WATCH_SUPPRESS_LAUNCH_BROWSER=true
